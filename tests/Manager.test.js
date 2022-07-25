b534f9012295a73bd27d76540d79d267d78e7118
Matthew <castoreno_05@Yessicas-MacBook-Pro.local>
2022-07-24 22:27:50 -0500

    Added tests for Manager.

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,11 +1,32 @@
-// Import Employee and Manager
-const Employee = require("../lib/Employee");
+// Import Manager
 const Manager = require("../lib/Manager");
-
-describe("Employee", () => {
+// Test getRole()
+describe("Manager", () => {
+    describe("getRole()", () => {
+        it('should return the role of manager', () => {
+            const test = "Manager";
+            const manager = new Manager("Matt", 1, "matthew.test@yahoo.com", 80);
+            expect(manager.getRole()).toEqual(test);
+        });
+    });
+});
+// Test officeNumber to see if it holds numbers
+describe("Manager", () => {
     describe("officeNumber", () => {
-        const number = 500;
-        const manager = new Manager("Matt", 1, "matthew.test@yahoo.com", number);
-        expect(manager.officeNumber).toEqual(number);
+        it('should set a vaule to office number', () => {
+            const value = 80;
+            const manager = new Manager("Steve", 1, "steve@yahoo.com", value);
+            expect(manager.officeNumber).toEqual(value);
+        });
+    });
+});
+// Test getOfficeNumber()
+describe("Manager", () => {
+    describe("getOfficeNumber()", () => {
+        it('should return a value for an office number', () => {
+            const value = 80;
+            const manager = new Manager("Yessica", 1, "yessicaC@yahoo.com", value);
+            expect(manager.getOfficeNumber()).toEqual(value);
+        });
     });
 });
