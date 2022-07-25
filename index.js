74d74f8617e48e92a50b821f97a884bc1b7b73e5
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-25 12:20:19 -0500

    Fixed syntax error on import variables. Added questions for the user. Once the role has been selected a prompt depending on the role will require user input.

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
-const Employee = require('Employee')
-const Engineer = require('Engineer')
-const Intern = require('Intern')
-const Manager = require('Manager')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const path = require("path");
+
+async function renderEmployee() {
+
+    let questions = [{
+        type: "input",
+        name: "employeeName",
+        message: "Provide an employee name"
+    },
+    {
+        type: "input",
+        name: "employeeEmail",
+        message: "Provide the employee's Email"
+    },
+    {
+        type: "input",
+        name: "employeeId",
+        message: "Provide the employee's Id"
+    },
+    {
+        type: "list",
+        name: "selectedEmployee",
+        message: "Which role will this employee have?",
+        choices: ["Manager", "Engineer", "Intern"]
+    }];
+
+    var answers = await inquirer.prompt(questions);
+
+    if (answers.selectedEmployee === "Manager") {
+        let manager = await inquirer.prompt({
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter managers office number"
+        });
+
+        let officeNumber = manager.officeNumber;
+        // console.log(officeNumber);
+        // Need to add officeNumber to Manager
+    }
+    else if (answers.selectedEmployee === "Engineer") {
+        let engineer = await inquirer.prompt({
+            type: 'input',
+            name: 'github',
+            message: 'Enter Github for Engineer'
+        });
+
+        let github = engineer.github;
+        // console.log(github);
+        // Need to add github to Engineer
+    }
+    else if (answers.selectedEmployee === "Intern") {
+        let intern = await inquirer.prompt({
+            type: 'input',
+            name: 'school',
+            message: 'Enter School Name for Intern'
+        });
+
+        let school = intern.school;
+        // console.log(school);
+        // Need to add school to Intern
+    }
+    else {
+        return false;
+    };
+
+}
+
+renderEmployee();
